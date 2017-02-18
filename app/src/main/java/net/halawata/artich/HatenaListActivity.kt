d20b312353c5c41c788f7a19202e85d1af8947d6
halawata13 <rsuzuki13@gmail.com>
2017-02-18 16:30:52 +0900

    first

---
+++ app/src/main/java/net/halawata/artich/HatenaListActivity.kt
@@ -0,0 +1,12 @@
+package net.halawata.artich
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class HatenaListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list)
+    }
+}
