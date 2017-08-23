3335df6b01ac31f14dc225a9b7299bee22c6607d
halawata13 <rsuzuki13@gmail.com>
2017-08-23 15:49:31 +0900

    Change default menu items

--- app/src/main/java/net/halawata/artich/model/DatabaseHelper.kt
+++ app/src/main/java/net/halawata/artich/model/DatabaseHelper.kt
@@ -12,9 +12,16 @@ class DatabaseHelper(val context: Context): SQLiteOpenHelper(context, "artich.db
 
     override fun onCreate(p0: SQLiteDatabase?) {
         p0?.execSQL("CREATE TABLE t_menu_common (id INTEGER PRIMARY KEY, name TEXT)")
-        p0?.execSQL("INSERT INTO t_menu_common(id, name) VALUES(1, 'Kotlin')")
-        p0?.execSQL("INSERT INTO t_menu_common(id, name) VALUES(2, 'Swift')")
-        p0?.execSQL("INSERT INTO t_menu_common(id, name) VALUES(3, 'JavaScript')")
+        p0?.execSQL("INSERT INTO t_menu_common(id, name) VALUES(1, 'JavaScript')")
+        p0?.execSQL("INSERT INTO t_menu_common(id, name) VALUES(2, 'Ruby')")
+        p0?.execSQL("INSERT INTO t_menu_common(id, name) VALUES(3, 'Python')")
+        p0?.execSQL("INSERT INTO t_menu_common(id, name) VALUES(4, 'iOS')")
+        p0?.execSQL("INSERT INTO t_menu_common(id, name) VALUES(5, 'PHP')")
+        p0?.execSQL("INSERT INTO t_menu_common(id, name) VALUES(6, 'Rails')")
+        p0?.execSQL("INSERT INTO t_menu_common(id, name) VALUES(7, 'Android')")
+        p0?.execSQL("INSERT INTO t_menu_common(id, name) VALUES(8, 'Swift')")
+        p0?.execSQL("INSERT INTO t_menu_common(id, name) VALUES(9, 'Java')")
+        p0?.execSQL("INSERT INTO t_menu_common(id, name) VALUES(10, 'AWS')")
 
         p0?.execSQL("CREATE TABLE t_menu_hatena (id INTEGER PRIMARY KEY, name TEXT)")
         p0?.execSQL("CREATE TABLE t_menu_qiita (id INTEGER PRIMARY KEY, name TEXT)")
